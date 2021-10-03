ef666268da227331599b7ae669f96d9b536bd7de
DragneilDaedalus <91740381+DragneilDaedalus@users.noreply.github.com>
2021-10-03 17:56:58 +0700

    Add files via upload

---
+++ Day 2/index(hw-rev).js
@@ -0,0 +1,100 @@
+// 3.a) print 0-6
+
+/*
+    for (let i=0; i<7; i++){
+    console.log(i);
+} 
+*/
+
+//3.b) print out 0-n. n is input
+
+/*
+{
+    let n = prompt('enter the number');
+    for(let i=0; i<n ; i++){
+        console.log(i);
+    }
+}
+*/
+
+//3.c) print out 3-n. n is input
+
+/*
+{
+    let i=3;
+    let n= prompt('Enter the number');
+    for(; i<n ; i++){
+        console.log(i);
+    }
+}
+*/
+
+// 3.d) print out n-c, n & c are input
+ 
+/*
+{
+    let n = prompt('Enter the first number');
+    let c = prompt('Enter the second number');
+    let i=n
+    for(; i<c; i++){
+        console.log(i);
+    }
+}
+*/
+
+//3.e) print out n-c(stepping by 3), n & c are input
+
+/*
+{
+    let n= Number(prompt('Enter the first number'));
+    let c= Number(prompt('Enter the second number'));
+    let i = n;
+    for(; i<c; i+=3){
+        console.log(i);
+    }
+}
+*/
+
+//3.f) Print out n-c(stepping by s), all three are input
+
+/*
+{
+    let n=Number(prompt('Enter the first number'));
+    let c=Number(prompt('Enter the second number'));
+    let s=Number(prompt('Enter the first number'));
+    for(;n<c;n+=s){
+        console.log(n);
+    }
+}
+*/
+
+//4) calculate the factorial from 1-n, n is input
+
+/*
+{
+    let fact=1;
+    let i=1;
+    let limit= Number(prompt('Enter the factorial number'));
+    for (; i<=limit; i++){
+        fact*=i;
+    }
+    alert(`The factorial of ${limit} is ${fact}`);
+}
+*/
+
+//5) Viewing 14y/o kids to adult contents
+
+/*
+{
+let age = Number(prompt('Enter your age'));
+if (age>14){
+    alert(`You're old enough to view this content!`);
+}else{
+    alert(`You're not old enough for this!`);
+}
+}
+*/
+
+
+//6)
+
---
+++ Day 2/index(study).js
@@ -0,0 +1,21 @@
+/* 1.a)
+- var and const are two other variables declaration
+1.b)
+- var is a global scope, let is a local scope 
+If var is declared, it can be used by any block scope (because it's global), while let is only used and declared inside of {}
+- var can be redeclared, while let can not (let variables can only updated)
+1.c)
+- let can be updated, while const can not (const = constance)
+const variables are set as read-only types
+1.d)
+- All of them are used to declared variables
+- But var is not as common as let and const since 2016 because it has so many problems and can be buggy
+- We use let and const instead. let is normally used for every variables declaration. When it come to the variables that we dont want
+to accidentally change it, we use const to set the value to read-only type.
+*/
+
+ /*
+ 2)
+ - The boolean datatype helps us to manipulate the condition
+ - It represent two types of values: True/False
+*/
