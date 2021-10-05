1d4b6a3db68feaf5863fa4615664c44a38337683
DragneilDaedalus <octymer2@gmail.com>
2021-10-06 02:31:15 +0700

    index.html

--- Day3/review.js
+++ Day3/review.js
@@ -1,29 +1,48 @@
 //4) Stimulating a clothes shop
 
+ /*
 let options = ['R', 'C', 'U', 'D'];
 let command = prompt('Hi there, welcome to shop admin panel, what do you want? (R, C, U, D)');
-let items = ['1. Jeans' , '2. T-shirt' , ' 3. Socks'];
-while (command!== options[0] && command!== options[1] && command!== options[2] && command!== options[3]){
-    alert('This command is not supported!');
-    command = prompt('Please re-enter your command!');
-} 
+let items = ['Jeans' , 'T-shirt' , 'Socks'];
+// while (command!== options[0] && command!== options[1] && command!== options[2] && command!== options[3]){
+//     alert('This command is not supported!');
+//     command = prompt('Please re-enter your command!');
+// } 
 
-// while (!options.includes(command)){
-//         alert('This command is not supported!');
-//         command = prompt('Hi there, welcome to shop admin panel, what do you want? (R, C, U, D)');
-//     } 
+while (!options.includes(command)){
+        alert('This command is not supported!');
+        command = prompt('Hi there, welcome to shop admin panel, what do you want? (R, C, U, D)');
+    } 
 
 while(command == 'C'){
     let newitem = prompt('Enter the new item:');
-    options.push(newitem);
+    items.push(newitem);
     alert('Done!');
-    command = prompt('Please re-enter your command!');
-    // return items.push(newitem);
+    command = prompt('Please re-enter your command:');
 }
 
+if (command == 'U'){
+    let newitem = prompt('Enter the update item:');
+    let order = prompt('Enter the position you want to update:')
+    items[order]= newitem;
+    alert('Done!')
+    command = prompt('Please re-enter your command');
+}
+
+if (command == 'D'){
+    let order = prompt('Please enter the position you want to remove:');
+    items.splice(order,1);
+    alert('Done!');
+    command = prompt ('Please re-enter your value:');
+}
 
 if(command == 'R'){
-    for (let i=0; i<items.length ; i++){
-      alert(`The items list are: \n ${items[i]}`);
+    // for (let i=0; i<items.length ; i++){
+    //   alert(`The items list are: \n ${items[i]}`);
+    console.log('The items list are:');
+    // console.log(items.join());
+    for (let i=0; i< items.length ; i++){
+        console.log(items[i]);
     }
 }
+*/
---
+++ Day3/serious.js
@@ -0,0 +1,50 @@
+// 5) Sum an array which the elements are input
+
+/*
+// let arr = prompt('Please enter your number:').split(','); 
+// We put a comma here so the the array wont take COMMA as an element
+// But this only return us a string array, we need an array of numbers
+// let input = prompt ('Pls enter your numbers'); (1)
+// let arr = input.split(',').map(Number); (2)
+let arr = prompt ('Please enter your numbers:').split(',').map(Number);
+// This is how to turn a string array into a number array
+// We just combine the (1) & (2) to shorten the code
+let sum = 0;
+for (let i = 0 ; i < arr.length ; i++){
+    sum += arr[i];
+}
+// console.log(sum);
+console.log(sum);
+*/
+
+//6) Find the smallest number in an array, the elements are iput
+
+/*
+let arr = prompt('Please enter your number').split(',').map(Number);
+// As performed above 
+alert('The smallest number is ' + Math.min(...arr));
+// This '...' is called destructuring an assignment, which helps us extract data from arrays or objects into distinct variables
+*/
+
+//7) Create an array, elements are input, check whether another input number is in the array
+
+let arr = prompt('Please enter your array:').split(',').map(Number);
+while (arr.length<5){
+    alert('The array must contains at least 5 elements!');
+    arr = prompt('Please enter at least 5 elements:').split(',').map(Number);
+}
+console.log(arr);
+
+let check = Number(prompt('Enter a random number:'));
+while (!check){
+    alert('Not a number!');
+    check = Number(prompt('Please enter a number!'));
+}
+if (!arr.includes(check)){
+    alert(`${check} is Not found in the array!`);
+}else{
+    alert(`${check} is FOUND in the array at index ${arr.indexOf(check)}`);
+}
+// indexOf() allows us to find the position of an element in the array
+// if we enter a number that is not in the array inside bracket, we will get -1 as a result
+// There are fromIndex() & lastIndexOf() that are similiar usage to indexOf()
