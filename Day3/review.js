edc637758e364235a3cc4247b4647e53c6eec04a
DragneilDaedalus <octymer2@gmail.com>
2021-10-08 00:09:27 +0700

    index.html

--- Day3/review.js
+++ Day3/review.js
@@ -1,42 +1,38 @@
 //4) Stimulating a clothes shop
 
- /*
+/*
 let options = ['R', 'C', 'U', 'D'];
-let command = prompt('Hi there, welcome to shop admin panel, what do you want? (R, C, U, D)');
+let command = prompt('Hi there, welcome to shop admin panel, what do you want (C, R, U, D)?');
 let items = ['Jeans' , 'T-shirt' , 'Socks'];
-// while (command!== options[0] && command!== options[1] && command!== options[2] && command!== options[3]){
-//     alert('This command is not supported!');
-//     command = prompt('Please re-enter your command!');
-// } 
 
-while (!options.includes(command)){
-        alert('This command is not supported!');
-        command = prompt('Hi there, welcome to shop admin panel, what do you want? (R, C, U, D)');
-    } 
-
-if (command == 'C'){
-    let newitem = prompt('Enter the new item:');
-    items.push(newitem);
-    alert('Done!');
-    command = prompt('Please re-enter your command:');
-}else if (command == 'U'){
-    let newitem = prompt('Enter the update item:');
-    let order = prompt('Enter the position you want to update:')
-    items[order]= newitem;
-    alert('Done!')
-    command = prompt('Please re-enter your command');
-} else if (command == 'D'){
-    let order = prompt('Please enter the position you want to remove:');
-    items.splice(order,1);
-    alert('Done!');
-    command = prompt ('Please re-enter your value:');
+while(true){
+    while(!options.includes(command)){
+    alert('This command is not supported!');
+    command = prompt('Please re enter your command');
 }
-
-if(command == 'R'){
-    
-    console.log('The items list are:');
-    for (let i=0; i< items.length ; i++){
-        console.log(`${i+1}. ${items[i]}`);
+    if (command == 'C'){
+        let newitem = prompt('Enter the new item:');
+        items.push(newitem);
+        alert('Done!');
+        command = prompt('Please re-enter your command:');
+    }else if(command == 'U'){
+        let newitem = prompt('Enter the update item:');
+        let order = prompt('Enter the position you want to update:')
+        items[order]= newitem;
+        alert('Done!')
+        command = prompt('Please re-enter your command');
+    }else if(command == 'D'){
+        let order = prompt('Please enter the position you want to remove:');
+        items.splice(order,1);
+        alert('Done!');
+        command = prompt ('Please re-enter your command:');
+    } else if(command == 'R'){
+        console.log('The items list are:');
+        for (let i=0; i< items.length ; i++){
+            console.log(`${i+1}. ${items[i]}`);
+    }
+    break;
     }
 }
 */
+    
