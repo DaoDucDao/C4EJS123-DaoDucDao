5fbaf5398a158392b9b99573c384b38650572420
DragneilDaedalus <octymer2@gmail.com>
2021-10-31 22:26:16 +0700

    index.html

--- Day10-test/script.js
+++ Day10-test/script.js
@@ -1,26 +1,43 @@
-const urlApi = 'https://tobitheme.net/api/characters.json';
-fetch(urlApi)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(harryPotter) {
-        console.log(harryPotter);
-        let nhanVat = harryPotter.data;
-        console.log(nhanVat);
-        let htmls = nhanVat.map(function(nhanVat) {
-            return `<div>
-            <div>
-            <ul>
-            <h2>${nhanVat.name}</h2>
-            <p>${nhanVat.house}</p>
-            </ul>
-            </div>
-            <div><img src="${nhanVat.image}" alt="${nhanVat.name}"></div>
-            </div>`;
-        });
-        let html = htmls.join('');
-        document.getElementById('character').innerHTML = html;
-    })
-    .catch(function(err) {
-        console.log('Lỗi');
-    })
+const charactersList = document.getElementById('charactersList');
+const search = document.getElementById('search');
+let hpCharacters = {};
+
+search.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+
+    const filteredCharacters = hpCharacters.filter((character) => {
+        return (
+            character.name.toLowerCase().includes(searchString) ||
+            character.house.toLowerCase().includes(searchString)
+        );
+    });
+    displayCharacters(filteredCharacters);
+});
+
+const loadCharacters = async() => {
+    try {
+        const res = await fetch('http://hp-api.herokuapp.com/api/characters');
+        hpCharacters = await res.json();
+        displayCharacters(hpCharacters);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+
+const displayCharacters = (characters) => {
+    const htmlString = characters
+        .map((character) => {
+            return `
+            <li class="character">
+                <h2>${character.name}</h2>
+                <p>House: ${character.house}</p>
+                <img src="${character.image}"></img>
+            </li>
+        `;
+        })
+        .join('');
+    charactersList.innerHTML = htmlString;
+};
+
+loadCharacters();
