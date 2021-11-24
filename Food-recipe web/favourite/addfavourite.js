2b0e8a95a60c0345bb7f4f3be1229b408eea0948
DragneilDaedalus <octymer2@gmail.com>
2021-11-25 00:52:03 +0700

    index.html

--- Food-recipe web/favourite/addfavourite.js
+++ Food-recipe web/favourite/addfavourite.js
@@ -1,5 +1,12 @@
+// const favRecipe = [];
+
+
+// window["favRecipe"] = eval("(" + localStorage.getItem("favRecipe") + ")");
+// window["pushIntoArray"] = eval(localStorage.getItem("pushIntoArray"));
+
+favListItems = document.getElementById('meal-fav-items');
+
 function addEventForAddToFavBtn() {
-    const favRecipe = [];
     const Favlist = document.getElementById('action_add_favourite');
     const getFav = document.querySelectorAll('#add-to-Fav')
 
@@ -13,38 +20,31 @@ function addEventForAddToFavBtn() {
             fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
                 .then(response => response.json())
                 .then(data => {
-                    console.log(data.meals);
+                    console.log(data.meals); // check what is the data meal
                     const arrId = favRecipe.map(recipe => recipe.idMeal);
                     if (data.meals.length > 0 && !arrId.includes(data.meals[0].id)) {
-                        favRecipe.push(data.meals[0])
+                        favRecipe.push(data.meals[0]);
+                        console.log(favRecipe); //check the array 
                     }
-
-
-                //    for ( recipe of favRecipe) {
-                //        if (favRecipe.includes(data.meals)){
-
-                //        }
-                //    }
-                    
                 });
     }
 }
 
-// const addFavouriteRecipe = (recipe) => {
-//     const newFavouritelist = [ ...favRecipe];
-//     setFavourites(newFavouritelist);
-//     return (
-//         <div className='container-fluid recipe-app'>
-//             <div className='row'>
-//                 <Recipelist
-//                     handleFavouritesClick={addFavouriteRecipe}
-//                 />
-//             </div>
-//             <div className='row d-flex align-items-center mt-4 mb-4'>
-//                 <REcipeListHeading heading='Favourites' />
-//             </div>
-//         </div>
-//     );
-// }
+let htmlFav = '';
+for (let i =0 ; i < favRecipe.length; i++){     // push the element in the array into Favourite html
+    html1 += `
+                    <div class = "meal-item" data-id = "${favRecipe[i].idMeal}">
+                        <div class = "meal-img">
+                            <img src = "${favRecipe[i].strMealThumb}" alt = "food">
+                        </div>
+                        <div class = "meal-name">
+                            <h3>${favRecipe[i].strMeal}</h3>
+                            <a href = "#" class = "recipe-btn">Get Recipe</a>
+                            <button type="button" class ="remove-fav-btn" id = "remove-from-Fav">Remove from favourite</button>
+                        </div>
+                    </div>
+                `;
+}
 
-// export default addfavourite; 
+favListItems.innerhtml= htmlFav;  // overwrite the html with above content
+
