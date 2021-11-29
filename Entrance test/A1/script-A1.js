22a33bd0f7c61df018c2ce4f9ef3f243bfcfa2e4
DragneilDaedalus <octymer2@gmail.com>
2021-11-29 20:44:00 +0700

    index.html

---
+++ Entrance test/A1/script-A1.js
@@ -0,0 +1,11 @@
+const arr = ['BINH','HUNG', 'PHUOC', 'CAO', 'KHANH'];
+const arr1 = [];
+let length;
+
+// for (let name of arr){
+//     length = name.length;
+//     if(length=arr1[0].length){
+        
+//     }   
+//     console.log(arr1);
+// }
